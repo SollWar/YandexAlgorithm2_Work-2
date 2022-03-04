91fbe300f2602c764ff0a73e81644e511f2bbb72
Denisov Nikita <sollwarmorgrind@gmail.com>
2022-03-04 15:20:42 +0300

    Задание А готово

---
+++ src/main/kotlin/eqMax/MainEqMax.kt
@@ -0,0 +1,23 @@
+package eqMax
+
+fun main() {
+    val scan = java.util.Scanner(System.`in`)
+    val array: ArrayList<Int> = arrayListOf(scan.nextInt())
+    var input = array[0]
+    var max = input
+
+    while (input != 0) {
+        input = scan.nextInt()
+        array.add(input)
+        if (max < input) max = input
+    }
+    println(eqMax(array, max))
+}
+
+fun eqMax(array: ArrayList<Int>, max: Int): Int {
+    var count = 0
+    repeat(array.size) {
+        if (array[it] == max && max != 0) count ++
+    }
+    return count
+}
